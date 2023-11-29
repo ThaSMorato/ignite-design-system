d8d31913ddb844487b6d42e98e6c83269c69d56b
ThaSMorato <tha.s.morato@gmail.com>
2023-11-29 14:50:37 -0300

    wirh decorator

--- packages/docs/src/stories/TextInput.stories.tsx
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -1,9 +1,22 @@
 import type { StoryObj, Meta } from '@storybook/react'
-import { TextInput, TextInputProps } from '@ignite-ui/react'
+import { Box, Text, TextInput, TextInputProps } from '@ignite-ui/react'
 
 export default {
   title: 'Form/TextInput',
   component: TextInput,
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size="sm">TextInput</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
 } as Meta<TextInputProps>
 
 export const Primary: StoryObj<TextInputProps> = {
