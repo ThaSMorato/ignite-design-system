1098c81ac8d5d1da3ecca3dec2f48795627ff979
ThaSMorato <tha.s.morato@gmail.com>
2023-11-29 11:50:52 -0300

    text component

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ea suscipit sit? Voluptates dolorem, aut laborum eveniet ipsum pariatur velit error iure repudiandae accusantium, expedita voluptatem officia quidem. Sit, animi.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
